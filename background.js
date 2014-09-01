d1d7ea66821eae9b1ad5c53d02f235f0712de115
Sahaj Sawhney <sahaj.sawhney@gmail.com>
2014-09-01 23:05:19 +0530

    added icons

--- background.js
+++ background.js
@@ -5,8 +5,10 @@ chrome.browserAction.onClicked.addListener(function(tab) {
     if (tabset) {
 	chrome.tabs.move(tabId, {'windowId': tab.windowId, index: -1});
 	tabset = false;
+	chrome.browserAction.setIcon({path: {'19': 'icon19in.png', '38': 'icon38in.png'}});
     } else {
 	tabId = tab.id;
 	tabset = true;
-    }
+	chrome.browserAction.setIcon({path: {'19': 'icon19out.png', '38': 'icon38out.png'}});
+}
 });
