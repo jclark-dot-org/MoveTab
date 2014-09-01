b247347e9244b4797bb07b44f3d581ff03c391cc
Sahaj Sawhney <sahaj.sawhney@gmail.com>
2014-09-01 23:40:22 +0530

    better style

--- background.js
+++ background.js
@@ -1,14 +1,24 @@
 tabset = false;
 tabId = 0;
 
+function setTab(id) {
+    tabId = id;
+    tabset = true;
+    chrome.browserAction.setIcon({path: {'19': 'icon19out.png', '38': 'icon38out.png'}});
+}
+
+function moveTab(windowId) {
+    chrome.tabs.move(tabId, {'windowId': windowId, index: -1});
+    tabset = false;
+    chrome.browserAction.setIcon({path: {'19': 'icon19in.png', '38': 'icon38in.png'}});
+}
+
 chrome.browserAction.onClicked.addListener(function(tab) {
     if (tabset) {
-	chrome.tabs.move(tabId, {'windowId': tab.windowId, index: -1});
-	tabset = false;
-	chrome.browserAction.setIcon({path: {'19': 'icon19in.png', '38': 'icon38in.png'}});
+	moveTab(tab.windowId);
     } else {
-	tabId = tab.id;
-	tabset = true;
-	chrome.browserAction.setIcon({path: {'19': 'icon19out.png', '38': 'icon38out.png'}});
-}
+	if (tab.hasOwnProperty('id')) {
+	    setTab(tab.id);
+	}
+    }
 });
